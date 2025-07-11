bcb39b6d2f5a44de61a76df1ebdfab2dd2999116
Ilia Eskin <devulusha@gmail.com>
2025-07-11 15:52:06 +0500

    fix: added states with colours

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ import MainContent from './components/MainContent/MainContent';
 function App() {
     const [selectedItem, setSelectedItem] = useState(null);
 
+    const [devicesStatus, setDevicesStatus] = useState({
+        "ИБП 1": "good",       // Зеленый
+        "ИБП 2": "warning",    // Желтый
+        "ИБП 3": "critical"    // Красный
+    });
+
+// Цвета для разных статусов
+    const statusColors = {
+        good: "#4CAF50",     // Зеленый
+        warning: "#FFC107",  // Желтый
+        critical: "#F44336"  // Красный
+    };
+
     const handleSelectItem = (title) => {
         console.log("App received selection:", title);
         setSelectedItem(title);
