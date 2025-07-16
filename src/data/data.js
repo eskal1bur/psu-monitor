355ad1bcfe93e4062a42a0958050e0768830722e
Ilia Eskin <devulusha@gmail.com>
2025-07-16 11:23:12 +0500

    refactor: changed data for 4th ups

--- src/data/data.js
+++ src/data/data.js
@@ -18,17 +18,44 @@ export const initialDevicesData = {
     "ИБП 2": {
         status: "warning",
         load: 15,
-        temperature: 42
+        temperature: 42,
+
+        workMode: "networkPowered",
+        inputVoltage: 232,
+        outputVoltage: 232,
+        inputFrequency: 50,
+
+        batteryStatus: "critical",
+        remainingBatteryCapacity: 20,
+        batteryVoltage: 26.5
     },
     "ИБП 3": {
         status: "critical",
         load: 28,
-        temperature: 38
+        temperature: 38,
+
+        workMode: "networkPowered",
+        inputVoltage: 232,
+        outputVoltage: 232,
+        inputFrequency: 50,
+
+        batteryStatus: "critical",
+        remainingBatteryCapacity: 20,
+        batteryVoltage: 26.5
     },
     "ИБП 4": {
         status: "critical",
         load: 5,
-        temperature: 31
+        temperature: 31,
+
+        workMode: "off",
+        inputVoltage: 232,
+        outputVoltage: 232,
+        inputFrequency: 50,
+
+        batteryStatus: "warning",
+        remainingBatteryCapacity: 50,
+        batteryVoltage: 26.5
     }
 };
 
